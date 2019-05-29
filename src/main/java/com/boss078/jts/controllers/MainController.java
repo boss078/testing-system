0f0520efce412199b537d2d9a40309a059037e66
unknown <izgachev01@mail.ru>
2019-05-29 19:48:51 +0300

    minor changes

--- src/main/java/com/boss078/jts/controllers/MainController.java
+++ src/main/java/com/boss078/jts/controllers/MainController.java
@@ -1,14 +1,8 @@
 package com.boss078.jts.controllers;
 
-import com.boss078.jts.entities.Test;
-import com.boss078.jts.repositories.TestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
